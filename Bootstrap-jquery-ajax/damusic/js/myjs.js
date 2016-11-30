ac2e79fae44c576e70e7e96ff67ccb49878d483d
tahtty <aahtty@espol.edu.ec>
2016-11-30 01:12:20 -0500

    Falta arreglar el length

--- Bootstrap-jquery-ajax/damusic/js/myjs.js
+++ Bootstrap-jquery-ajax/damusic/js/myjs.js
@@ -30,8 +30,9 @@ function tops(artista) {
 }
 
 function getyoutube(artista,canciones) {
-	link = "https://www.googleapis.com/youtube/v3/search"
-	query =[]
+	link = "https://www.googleapis.com/youtube/v3/search";
+	query =[];
+	var re=[];
 	for (var i = canciones.length - 1; i >= 0; i--) {
 		busqueda = artista.nombre+" "+canciones[i];
 		query.push(busqueda);
@@ -39,16 +40,41 @@ function getyoutube(artista,canciones) {
 	videos =[]
 	for (var i = query.length - 1; i >= 0; i--) {
 		$.getJSON(link, {jsoncallback: '?',key: k ,q: query[i] ,part:"snippet",type: "video"}, function(res) {
-			console.log(res)
+			console.log(res);
+			console.log(re);
+			re.push(res);
+			console.log(re);
 	});
 	}
-	llenar(res,artista);
+	console.log(re);
+	llenar(re,artista);
 }
 
 function llenar(r,a){
+	$("#imagen").attr("src",a.portada.url);
+	$("#informacion h2").html(a.nombre);
+	$("#informacion a").attr("href",a.spotify);
+	$("#informacion p").html(a.seguidores+" seguidores");
+	console.log(r.length);
+	for (var i = 1; i <=r.length; i++) {
+		var vlink="https://www.youtube.com/embed/"+r[i-1].items[0].id.videoId;
+		console.log(vlink);
+		var iframe=document.createElement("iframe");
+		$(iframe).attr("width","400");
+		$(iframe).attr("height","300");
+		$(iframe).attr("src",vlink);
+		console.log(r[i].items[0].snippet.thumbnails.title);
+		$("#p"+i+" panel .panel-default .panel-footer p").html(r[i].items[0].snippet.thumbnails.title);
+		$("#p"+i+" panel .panel-default .panel-body").append(iframe);
+	}
 	
 }
 
 $(document).ready(function() {
+	/*$("#inputTags +button").click(function(e) {
+	var inp=$("#input").val();
+	console.log(inp);
+	obtenerArtista(inp);
+});*/
 	obtenerArtista("ColdPlay");
 });
